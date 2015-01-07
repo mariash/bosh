024645303870878705a68257ce932822ad3988eb
Colin OByrne <cobyrne@pivotal.io>
2015-01-06 17:16:04 -0800

    remove specs that restart director
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- spec/integration/hm_2_spec.rb
+++ spec/integration/hm_2_spec.rb
@@ -17,20 +17,4 @@ describe 'health_monitor: 2', type: :integration do
     director.vm('foobar/0').kill_agent
     expect(director.wait_for_vm('foobar/0', 150)).to be_nil
   end
-
-  # ~2m
-  it 'resurrects stateful nodes if fix_stateful_nodes director option is set' do
-    current_sandbox.director_fix_stateful_nodes = true
-    current_sandbox.restart_director
-
-    deployment_hash = Bosh::Spec::Deployments.simple_manifest
-    deployment_hash['jobs'][0]['instances'] = 1
-    deployment_hash['jobs'][0]['persistent_disk'] = 20_480
-    deploy_simple(manifest_hash: deployment_hash)
-
-    original_vm = director.vm('foobar/0')
-    original_vm.kill_agent
-    resurrected_vm = director.wait_for_vm('foobar/0', 150)
-    expect(resurrected_vm.cid).to_not eq(original_vm.cid)
-  end
 end
