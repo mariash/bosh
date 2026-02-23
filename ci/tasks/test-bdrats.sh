967e0b44bcd3eae3d5e31d65711f6eed31b1c972
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-02-23 19:54:49 +0000

    Use remote stemcell

--- ci/tasks/test-bdrats.sh
+++ ci/tasks/test-bdrats.sh
@@ -36,7 +36,6 @@ for attempt in $(seq 1 $MAX_ATTEMPTS); do
     -o bbr.yml \
     -o local-bosh-release-tarball.yml \
     -o hm/disable.yml \
-    -o local-stemcell.yml \
     -v local_bosh_release="${BOSH_RELEASE_PATH}" \
     -v local_stemcell_url="${STEMCELL_PATH}" \
     -v local_stemcell_sha1="${STEMCELL_SHA1}"; then
