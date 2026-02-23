1252ef72fe633944e728f2707dfe1cd5ff9dea47
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-02-23 22:01:09 +0000

    Use local noble

--- ci/tasks/test-bdrats.sh
+++ ci/tasks/test-bdrats.sh
@@ -13,10 +13,19 @@ export BBR_BINARY_PATH="${PWD}/bbr-binary/bbr"
 cp bbr-cli-binary/bbr-[0-9]*-linux-amd64 "${BBR_BINARY_PATH}"
 chmod +x "${BBR_BINARY_PATH}"
 
-export OVERRIDDEN_BOSH_DEPLOYMENT="${src_dir}/bosh-deployment"
-if [[ -e ${OVERRIDDEN_BOSH_DEPLOYMENT}/bosh.yml ]];then
-  export BOSH_DEPLOYMENT_PATH=${OVERRIDDEN_BOSH_DEPLOYMENT}
-fi
+export BOSH_DEPLOYMENT_PATH="${src_dir}/bosh-deployment"
+
+STEMCELL_PATH="${STEMCELL_PATH:-file://${PWD}/stemcell/$(basename stemcell/*.tgz)}"
+STEMCELL_SHA1="${STEMCELL_SHA1:-$(sha1sum "${PWD}/stemcell/$(basename stemcell/*.tgz)" | awk '{print $1}')}"
+
+cat > "${BOSH_DEPLOYMENT_PATH}/local-stemcell.yml" <<'OPSEOF'
+- name: stemcell
+  path: /resource_pools/name=vms/stemcell?
+  type: replace
+  value:
+    url: ((local_stemcell_url))
+    sha1: ((local_stemcell_sha1))
+OPSEOF
 
 # Retry start-bosh up to 3 times
 MAX_ATTEMPTS=3
@@ -27,7 +36,10 @@ for attempt in $(seq 1 $MAX_ATTEMPTS); do
     -o bbr.yml \
     -o local-bosh-release-tarball.yml \
     -o hm/disable.yml \
-    -v local_bosh_release="${BOSH_RELEASE_PATH}"; then
+    -o local-stemcell.yml \
+    -v local_bosh_release="${BOSH_RELEASE_PATH}" \
+    -v local_stemcell_url="${STEMCELL_PATH}" \
+    -v local_stemcell_sha1="${STEMCELL_SHA1}"; then
     
     echo "BOSH started successfully, sourcing environment..."
     source /tmp/local-bosh/director/env
@@ -48,11 +60,10 @@ done
 
 source /tmp/local-bosh/director/env
 
-STEMCELL_PATH="${PWD}/stemcell/$(basename stemcell/*.tgz)"
 BOSH_SSH_KEY="$(bosh int /tmp/local-bosh/director/creds.yml --path /jumpbox_ssh/private_key --json | jq .Blocks[0])"
 BOSH_HOST="${BOSH_ENVIRONMENT}"
 
-stemcell_os="ubuntu-jammy"
+stemcell_os="ubuntu-noble"
 bosh_ca_cert_json_value="$(awk '{printf "%s\\n", $0}' "${BOSH_CA_CERT}")"
 
 cat > integration-config.json <<EOF
